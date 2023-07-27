0f26e8021640ce9652b97de066e7ce66e36281aa
pawel maryon <maryonpawel@gmail.com>
2023-07-27 16:33:11 +0100

    model improvementsC

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,12 +3,12 @@ class Pokemon < ApplicationRecord
 
   def self.save_pokemon_names_to_db
     names = PokemonService.new.get_all_pokemon_names
-    names.each{|pokemon| self.create!(name: pokemon['name'])}
+    names.each { |pokemon| create!(name: pokemon['name']) }
   end
 
   def save_pokemon_types_to_db
-    types = PokemonService.new.get_pokemon_by_name(self.name)['types']
-    types.each{|type| self.types.build(name: type['type']['name'])}
-    self.save
+    types = PokemonService.new.get_pokemon_by_name(name)['types']
+    types.each { |type| self.types.build(name: type['type']['name']) }
+    save
   end
 end
