cc99379cbd049dfbbee8559ad8bc91b8ff8cfd61
pawel maryon <maryonpawel@gmail.com>
2023-07-27 15:51:14 +0100

    saving records from API to database

--- app/services/pokemon_service.rb
+++ app/services/pokemon_service.rb
@@ -1,6 +1,5 @@
 # Purpose: To make API calls to the PokeAPI
 class PokemonService
-  include HTTParty
 
   def initialize
     self.class.base_uri Rails.configuration.pokeapi_base_url
@@ -8,12 +7,12 @@ class PokemonService
 
   def get_all_pokemon_names
     response = self.class.get('/pokemon')
-    JSON.parse(response.body)
+    JSON.parse(response.body)["results"]
   end
 
   def get_all_pokemon_types
     response = self.class.get('/type')
-    JSON.parse(response.body)
+    JSON.parse(response.body)["results"]
   end
 
   def get_pokemon_by_name(name)
