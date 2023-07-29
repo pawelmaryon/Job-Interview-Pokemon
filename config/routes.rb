21ecc4ac4aeb07d31f2c2a64d7027f8f43375556
pawel maryon <maryonpawel@gmail.com>
2023-07-29 10:58:26 +0100

    pokemon and types routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :pokemons, only: [:index, :show]
+  resources :types, only: [:index, :show]
 end
